8dcb4c8ae5388f5233988b9ddb5db815f3b03ac2
Zack Chrisman <zjchrisman@student.fullsail.edu>
2024-08-28 10:56:37 -0500

    added PartTime class

--- JS/script.js
+++ JS/script.js
@@ -9,6 +9,19 @@ class Employee {
 	}
 }
 
+class PartTime extends Employee {
+	constructor(name, age, payRate, hours) {
+		super(name, age);
+		this.payRate = payRate;
+		this.hours = hours;
+		this.employeeType = "PT";
+		this.calculatePay();
+	}
+	calculatePay() {
+		this.annualSalary = this.payRate * this.hours * 52;
+	}
+}
+
 //IIfE containing the main class
 (() => {
 	//new Main();
