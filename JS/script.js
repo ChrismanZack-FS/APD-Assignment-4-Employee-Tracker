742178e6f343b5089341359b44afa41f0152b276
Zack Chrisman <zjchrisman@student.fullsail.edu>
2024-08-28 10:50:22 -0500

    IIFE created as well as base class employee

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+import { logger } from "./utilities.js";
+
+//Employee class
+class Employee {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+		this.annualSalary = 0;
+	}
+}
+
+//IIfE containing the main class
+(() => {
+	//new Main();
+})();
---
+++ JS/utilities.js
@@ -0,0 +1,3 @@
+const logger = (value) => console.log(value);
+
+export { logger };
